b8d03a2ae1c55582250bc29045f2e6fa77ff58d0
supremekhaoz <luisliz2038@gmail.com>
2016-02-13 20:11:11 -0400

    Finishing Persistance

--- Twitter/User.swift
+++ Twitter/User.swift
@@ -28,36 +28,6 @@ class User: NSObject {
     }
     
     class var currentUser: User? {
-        /*
-        // data returned from the network response will typically be of type
-        // NSData (which is a buffer of bytes)
-        let responseData: NSData = // ... some value retrieved from the network response ...
-        
-        // Wrap our code in a do catch as our code might throw an exception which we need to handle
-        do {
-            // Start by converting the NSData to a dictionary - a dictionary for the entire response
-            if let responseDictionary = try NSJSONSerialization.JSONObjectWithData(responseData,
-                options:NSJSONReadingOptions(rawValue:0)) as? [String:AnyObject] {
-                    
-                    // Dip inside the response to find the "movies" key and get the array of movies
-                    if let movies = responseDictionary["movies"] as? [AnyObject] {
-                        
-                        // Get each movie dictionary from the array of movies
-                        for movie in movies {
-                            
-                            // Use the movie "title" key and "rating" key to get their values
-                            if let title = movie["title"] as? String {
-                                if let rating = movie["rating"] as? Double {
-                                    print("Title:\(title), rating:\(rating)")
-                                }
-                            }
-                        }
-                    }
-            }
-        } catch {
-            print("Error parsing JSON")
-        }*/
-        
         get {
             if _currentUser == nil {
                 let data = NSUserDefaults.standardUserDefaults().objectForKey(currentUserKey) as? NSData
@@ -77,18 +47,19 @@ class User: NSObject {
         
         set(user) {
             _currentUser = user
+            
             if _currentUser != nil {
                 //convert nsdictionary into nsdata
                 let data2 : NSData = NSKeyedArchiver.archivedDataWithRootObject(user!.dictionary)
                 do {
-                    if let data = try NSJSONSerialization.JSONObjectWithData(data2, options: NSJSONReadingOptions(rawValue:0)) as? NSDictionary	 {
-                        NSUserDefaults.standardUserDefaults().setObject(data, forKey: currentUserKey)
-                    }
+                    let data = try NSJSONSerialization.JSONObjectWithData(data2, options: NSJSONReadingOptions(rawValue:0))
+                    NSUserDefaults.standardUserDefaults().setObject(data, forKey: currentUserKey)
                 } catch {
-                    print("Error with JSON")
+                    print("Error in set user: \(error)")
                 }
             } else {
                 NSUserDefaults.standardUserDefaults().setObject(nil, forKey: currentUserKey)
+                
             }
             NSUserDefaults.standardUserDefaults().synchronize()
         }
